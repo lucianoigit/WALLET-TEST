eb6688e60cdcd519728c3ef7ff64c362efaffa55
lucianoigit <lucianoiriartegit@gmail.com>
2025-04-08 19:47:28 -0300

    chore(api): update Program.cs, settings and add Extensions folder

---
+++ API/Extensions/MigrationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using INFRAESTRUCTURE;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class MigrationExtensions
+    {
+        public static void ApplyMigration(this IApplicationBuilder app)
+        {
+            using IServiceScope scope = app.ApplicationServices.CreateScope();
+            using ApplicationDbContext _context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            // Verificar si hay migraciones pendientes antes de aplicarlas
+            var pendingMigrations = _context.Database.GetPendingMigrations();
+
+            if (pendingMigrations.Any())
+            {
+                Console.WriteLine("Applying pending migrations...");
+                _context.Database.Migrate();
+            }
+            else
+            {
+                Console.WriteLine("No pending migrations.");
+            }
+        }
+    }
+}
