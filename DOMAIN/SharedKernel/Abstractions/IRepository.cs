cf139128e89afdb2470f1fdb6f06838049538abd
lucianoigit <lucianoiriartegit@gmail.com>
2025-04-08 16:01:52 -0300

    feat(domain): add IRepository base interface

---
+++ DOMAIN/SharedKernel/Abstractions/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace DOMAIN.SharedKernel.Abstractions;
+
+public interface IRepository<TEntity, TEntityId>
+    where TEntity : Entity<TEntityId>
+    where TEntityId : class
+{
+    Task<TEntity?> GetByIdAsync(TEntityId id);
+    void Add(TEntity entity);
+    void Update(TEntity entity);
+    void Remove(TEntity entity);
+}
