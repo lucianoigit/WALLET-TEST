fe1035685e5ea8aece063873d1f57ec6ca8631ad
lucianoigit <lucianoiriartegit@gmail.com>
2025-04-08 16:40:59 -0300

    feat(application): add DependencyInjection

---
+++ APLICATION/Abstractions/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace APLICATION.Abstractions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+
+            config.AddOpenBehavior(typeof(RequestLoggingPipelineBehavior<,>));
+            config.AddOpenBehavior(typeof(ValidationPipelineBehavior<,>));
+            //config.AddOpenBehavior(typeof(QueryCachingBehavior<,>));
+        });
+
+        return services;
+    }
+}
