29ac0d0bad46dc545b1742ba21316555db06e6a3
lucianoigit <lucianoiriartegit@gmail.com>
2025-04-09 09:16:18 -0300

    feat(domain): add ReceivingWalletId to Movement entity and update migration

--- DOMAIN/Movements/Movement.cs
+++ DOMAIN/Movements/Movement.cs
@@ -7,21 +7,23 @@ public sealed class Movement : Entity<int>
     public int WalletId { get; private set; }
     public decimal Amount { get; private set; }
     public MovementType Type { get; private set; }
+    public int? ReceivingWalletId { get; private set; }
     public DateTime Date { get; private set; }
 
     private Movement() { }
 
-    private Movement(int walletId, decimal amount, MovementType type)
+    private Movement(int walletId,int receivingWalletId, decimal amount, MovementType type)
         : base(0) // El ID lo genera EF, usamos 0 como valor por defecto.
     {
         WalletId = walletId;
+        ReceivingWalletId = receivingWalletId;
         Amount = amount;
         Type = type;
         Date = DateTime.UtcNow;
     }
 
-    public static Movement Create(int walletId, decimal amount, MovementType type)
+    public static Movement Create(int walletId,int receivingWalletId, decimal amount, MovementType type)
     {
-        return new Movement(walletId, amount, type);
+        return new Movement(walletId, receivingWalletId, amount, type);
     }
 }
