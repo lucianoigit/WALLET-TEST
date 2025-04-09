9724e7ba3dc1c10324fca854162e7013821ad648
lucianoigit <lucianoiriartegit@gmail.com>
2025-04-09 09:16:53 -0300

    feat(domain): add domain error handler for Movement

---
+++ DOMAIN/Movements/MovementErrors.cs
@@ -0,0 +1,8 @@
+﻿using DOMAIN.SharedKernel.Primitives;
+
+namespace DOMAIN.Movements;
+
+public static class MovementErrors
+{
+    public static Error InvalidMovementType = Error.NotFound("Wallet.InvalidMovementType", "The Issuing wallet does not exist.");
+}
